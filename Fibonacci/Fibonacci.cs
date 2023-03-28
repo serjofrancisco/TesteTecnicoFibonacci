681ac67aa60c717373f50d9d85636143cf3795f8
Sergio Luis Francisco Pinheiro <spinheiro.lf@gmail.com>
2023-03-28 09:57:49 -0300

    cria fibbonacci recursiva

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciTest
+{
+    public class Fibonacci
+    {
+        public int FibonacciFunc(int n)
+        {
+            if (n <= 1)
+                return n;
+            return FibonacciFunc(n - 1) + FibonacciFunc(n - 2);
+        }
+    }
+}
--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FibonacciTest;
+
+namespace FibbonacciTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            Console.WriteLine("Digite uma posição");
+
+            string input = Console.ReadLine() ?? "";
+            int position = int.Parse(input);
+
+            int result = fibonacci.FibonacciFunc(position);
+
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
